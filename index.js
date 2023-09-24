19837d7722f03c6ad84854479e02a6c46b8f725f
Marsel <nowly2k@mail.ru>
2023-09-24 16:51:21 +0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const TelegramBot = require('')
+const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.MY_TOKEN
+const token = '6333179341:AAGDuXJXNCdCs-Teol4IUutsc6KGRV9C1Fk';
+const webAppUrl = 'https://jovial-crumble-d429d8.netlify.app/'
+const bot = new TelegramBot(token, { polling: true });
+
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'ниже появится кнопка заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{ text: 'Заполнить форму', web_app: { url: webAppUrl + '/form' } }]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Магазин', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
+                ]
+            }
+        })
+    }
+});
